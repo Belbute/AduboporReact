d667da039e70c8e4597b7186432bce835aa403c4
Belbute <76134896+Belbute@users.noreply.github.com>
2024-10-22 14:22:10 +0200

    Update Testimonials.tsx

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -4,20 +4,10 @@ import { TestimonialsData } from "../data/lists";
 const Testimonials: React.FC = () => {
   return (
     <div
-      className=" mx-auto py-40 px-4 sm:px-6 flex flex-col lg:flex-row gap-10 bg-app-secondary"
+      className="container mx-auto py-20 pt-30 px-4 sm:px-6 flex flex-col lg:flex-row gap-10 lg:flex-row-reverse"
       id="testimonials"
     >
-      {/* Testimonials Section (Left Side) */}
-      <div className="grid mx-auto grid-cols-1 gap-4 grid-rows-4 sm:grid-cols-2 lg:w-1/2 lg:grid-cols-6 lg:grid-rows-[100px_100px_100px_100px_100px_100px] lg:gap-y-3 lg:gap-x-3 lg:grid-flow-row">
-        <div className="bg-white p-8 rounded-xl shadow-lg flex flex-col items-center text-center lg:col-span-2 lg:row-span-3"></div>
-        <div className="bg-yellow-500 p-8 rounded-xl shadow-lg flex flex-col items-center text-center lg:row-span-3 lg:col-span-3"></div>
-        <div className="bg-red-500 p-8 rounded-xl shadow-lg flex flex-col items-center text-center lg:row-span-3 lg:col-span-3"></div>
-        <div className="bg-blue-500 p-8 rounded-xl shadow-lg flex flex-col items-center text-center lg:row-span-3 lg:col-span-2"></div>
-      </div>
-      <div className="hidden lg:block lg:w-5px"></div>
-      <div className="lg:w-[5px]"></div>
-      {/* Text Section (Right Side) */}
-      <div className=" mx-auto lg:w-1/3 flex flex-col justify-center text-textColors-light">
+            <div className=" mx-auto lg:w-1/3 flex flex-col justify-center text-textColors-light">
         <h2 className="text-4xl font-bold mb-7">What People Say About Us</h2>
         <p className="text-lg mb-4">
           Our clients have always been at the heart of our business. Here's what some of them have
@@ -28,6 +18,14 @@ const Testimonials: React.FC = () => {
           testimonials highlight our dedication to customer satisfaction.
         </p>
       </div>
+      {/* Testimonials Section (Left Side) */}
+      <div className="w-full grid mx-auto grid-cols-1 gap-4 sm:grid-rows-4 sm:grid-cols-2 lg:w-2/3 lg:grid-cols-6 lg:grid-rows-[100px_100px_100px_100px_100px_100px] lg:gap-y-3 lg:gap-x-3 lg:grid-flow-row">
+        <div className="min-h-60 bg-white p-8 rounded-xl items-center text-center lg:col-span-2 lg:row-span-3"></div>
+        <div className=" min-h-60 bg-yellow-500 p-8 col-span-1 rounded-xl items-center text-center lg:row-span-3 lg:col-span-3"></div>
+        <div className="min-h-60 bg-red-500 p-8 rounded-xl items-center text-center lg:row-span-3 lg:col-span-3"></div>
+        <div className="min-h-60 bg-blue-500 p-8 rounded-xl items-center text-center lg:row-span-3 lg:col-span-2"></div>
+      </div>
+      {/* Text Section (Right Side) */}
     </div>
   );
 };
