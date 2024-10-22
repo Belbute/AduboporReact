99a8ea66ed63c6ea730affd0ae3c0f2947b94457
Belbute <76134896+Belbute@users.noreply.github.com>
2024-10-22 16:45:20 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -23,13 +23,14 @@ export default function App() {
         </div>
 
         <ProductCategories />
-        <img
-          src={wave}
-          alt="Wave Separator"
-          style={{ width: "100%", height: "100%", verticalAlign: "middle" }}
-        />
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 220" className="text-app-secondary">
+  <path className="fill-current" fill-opacity="1" d="M0,96L60,101.3C120,107,240,117,360,133.3C480,149,600,171,720,165.3C840,160,960,128,1080,117.3C1200,107,1320,117,1380,122.7L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
+</svg>
         <div className="bg-app-secondary">
           <Testimonials />
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 220" className="text-app-main">
+  <path className="fill-current bg-app-secondary" fill-opacity="1" d="M0,96L60,101.3C120,107,240,117,360,133.3C480,149,600,171,720,165.3C840,160,960,128,1080,117.3C1200,107,1320,117,1380,122.7L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
+</svg>
         </div>
 
 
