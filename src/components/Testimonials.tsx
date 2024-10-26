fc8ba777605f2853d39069da57a4f7b2955a16bb
José Machado <belbute1999@gmail.com>
2024-10-26 23:35:16 +0100

    Testimonials rework started

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Relva from "../assets/SVG/Relva.svg";
+import Fertelizante from "../assets/SVG/Fertelizante.svg";
+import Passear from "../assets/SVG/Passear.png";
 import { TestimonialsData } from "../data/lists";
 import TestimonialCard from "./TestimonialCard";
 
 const Testimonials: React.FC = () => {
+  const [currentImage, setCurrentImage] = useState(0);
+  const images = [Relva, Fertelizante, Passear];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage((prevImage) => (prevImage + 1) % images.length);
+    }, 3000); // Change image every 3 seconds
+
+    return () => clearInterval(interval);
+  }, [images.length]);
+
   return (
     <div
-      className="container mx-auto py-20 pt-30 px-4 sm:px-6 flex flex-col lg:flex-row gap-10 lg:flex-row-reverse"
+      className="container mx-auto py-20 pt-30 px-4 sm:px-6 flex flex-col gap-10 lg:flex-row-reverse"
       id="testimonials"
     >
       <div className="mx-auto lg:w-1/3 flex flex-col justify-center text-textColors-light">
@@ -20,31 +34,18 @@ const Testimonials: React.FC = () => {
         </p>
       </div>
       {/* Testimonials Section (Left Side) */}
-      <div className="w-full grid mx-auto grid-cols-1 gap-4 sm:grid-rows-4 sm:grid-cols-2 lg:w-2/3 lg:grid-cols-6 lg:grid-rows-[100px_100px_100px_100px_100px_100px] lg:gap-y-3 lg:gap-x-3 lg:grid-flow-row">
-        <TestimonialCard
-          content="This is a wonderful service that I highly recommend!"
-          className="lg:col-span-2 lg:row-span-3"
-          imageUrl="src/assets/AduboporBackground2.3.png"
-          clovers={5}
-        />
-        <TestimonialCard
-          content="Exceptional quality and support from this company!"
-          className="lg:row-span-3 lg:col-span-3"
-          imageUrl="src/assets/AduboporBackground2.3.png"
-          clovers={4}
-        />
-        <TestimonialCard
-          content="We are absolutely thrilled with our experience."
-          className="lg:row-span-3 lg:col-span-3"
-          imageUrl="src/assets/AduboporBackground2.3.png"
-          clovers={5}
-        />
-        <TestimonialCard
-          content="Great products and an even better team behind them."
-          className="lg:row-span-3 lg:col-span-2"
-          imageUrl="src/assets/AduboporBackground2.3.png"
-          clovers={3}
-        />
+      <div className="mx-auto lg:w-2/3 w-full h-[400px] lg:h-[600px] flex justify-center items-center relative overflow-hidden">
+        {images.map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt=""
+            className={`absolute transition-opacity duration-1000 ease-in-out lg:w-[70%] h-auto object-contain -translate-x-[50%] lg:-translate-x-[75%] ${
+              index === currentImage ? "opacity-100" : "opacity-0"
+            }`}
+            style={{ top: 0, left: "50%" }}
+          />
+        ))}
       </div>
     </div>
   );
